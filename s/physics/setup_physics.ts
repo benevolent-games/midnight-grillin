0836ce0090a2cd72897ec5935a51b57ad895bcb8
zenkyu <przemekgg2002@gmail.com>
2023-08-20 22:51:32 +0200

    ignore: havok ts error until they fix it

--- s/physics/setup_physics.ts
+++ s/physics/setup_physics.ts
@@ -3,6 +3,7 @@ import "@babylonjs/core/Physics/physicsEngineComponent.js"
 import {HavokPlugin} from "@babylonjs/core"
 import {Scene} from "@babylonjs/core/scene.js"
 import {v3, V3} from "@benev/toolbox/x/utils/v3.js"
+// @ts-ignore
 import HavokPhysics from "@babylonjs/havok/lib/esm/HavokPhysics_es.js"
 
 export async function setupPhysics(
