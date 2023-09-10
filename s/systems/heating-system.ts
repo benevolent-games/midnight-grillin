06522cae45f2282e6a5e32d015411607f96fdba1
zenkyu <przemekgg2002@gmail.com>
2023-09-10 04:03:50 +0200

    refactor: heating system, prep systems

--- s/systems/heating-system.ts
+++ s/systems/heating-system.ts
@@ -1,38 +1,34 @@
-import {Scene} from "@babylonjs/core"
-
+import {Item} from "../scene-items/Item.js"
 import {Coal} from "../scene-items/pickables/coal.js"
 import {Steak} from "../scene-items/usables/steak.js"
 
 export class HeatingSystem {
-	heat_sources: Coal[] = [] // for now just coals, more will be added later
-	cookables: Steak[] = [] // for now just steak, more variety of food later
-	scene: Scene
+	#heat_sources: Coal[] = [] // for now just coals, more will be added later
+	#cookables: Steak[] = [] // for now just steak, more variety of food later
 
-	constructor(scene: Scene) {
-		this.scene = scene
-		this.cookables = this.get_cookables()
-		this.heat_sources = this.get_heat_sources()
+	constructor() {
 		setInterval(() => this.recalculate_heat_levels(), 1000)
 	}
 
 	recalculate_heat_levels() {
-		this.cookables.map(cookable =>
-			cookable.calculate_cooking(this.heat_sources)
+		this.#cookables.map(cookable =>
+			cookable.calculate_cooking(this.#heat_sources)
 		),
-		this.heat_sources.map(heat =>
-			heat.calculate_heat_level(this.heat_sources.filter(h => h !== heat))
+		this.#heat_sources.map(heat =>
+			heat.calculate_heat_level(this.#heat_sources.filter(h => h !== heat))
 		)
 	}
 
-	get_heat_sources() {
-		const heat_sources = this.scene.getMeshesById("heat-source").map(h => h.metadata) as Coal[] | null
-		if(heat_sources) return heat_sources
-			else return []
+	add_heat_source = (item: Item.Any) => {
+		if(item instanceof Coal) {
+			this.#heat_sources.push(item)
+		}
 	}
 
-	get_cookables() {
-		const cookables = this.scene.getMeshesById("cookable").map(c => c.metadata) as Steak[] | null
-		if(cookables) return cookables
-			else return []
+	add_cookable = (item: Item.Any) => {
+		if(item instanceof Steak) {
+			this.#cookables.push(item)
+		}
 	}
 }
+
--- s/utils/prepare-systems.ts
+++ s/utils/prepare-systems.ts
@@ -12,16 +12,19 @@ export function prepare_systems(scene: Scene, character: Character_capsule) {
 	const ui = AdvancedDynamicTexture.CreateFullscreenUI("myUI")
 	const item_handler = new ItemHandler(scene)
 	const scene_items = new SceneItemsControl()
-	const gui_control = new GuiControl(ui, scene)
+	const gui_control = new GuiControl(ui)
+	const heating_system = new HeatingSystem()
 
-	const heating_system = new HeatingSystem(scene)
-
-	scene_items.on_item_added(gui_control.create_guis)
+	scene_items.on_item_added(
+		gui_control.create_guis,
+		heating_system.add_heat_source,
+		heating_system.add_cookable
+	)
 
 	item_handler
-		.on_item_drop((gui_control.handle_ondrop_gui, character.handle_item_drop))
-		.on_item_pick((gui_control.handle_onpick_gui, character.handle_item_pick))
-		.on_item_equip((gui_control.handle_onequip_gui, character.handle_item_equip))
+		.on_item_drop(gui_control.handle_ondrop_gui, character.handle_item_drop)
+		.on_item_pick(gui_control.handle_onpick_gui, character.handle_item_pick)
+		.on_item_equip(gui_control.handle_onequip_gui, character.handle_item_equip)
 		.on_intersect_change(gui_control.handle_on_intersect_gui)
 		.on_item_use((equipped, intersected) => equipped.use(intersected))
 
